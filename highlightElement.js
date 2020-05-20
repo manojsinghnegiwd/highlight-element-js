be8b125935059b74ba865f1fde4c65fe0b4bf562
Manoj Singh Negi <justanothermanoj@gmail.com>
2020-05-20 22:06:51 +0530

    maded higligtedDiv a property

--- highlightElement.js
+++ highlightElement.js
@@ -10,17 +10,17 @@ window["HighlightElement"] = function (selector, config) {
 
     this.highlight = function () {
         dims = this.targetElement.getBoundingClientRect()
-		highlightedDiv = document.createElement("div")
-		highlightedDiv.style.borderRadius = `5px`
-		highlightedDiv.style.width = `${dims.width}px`
-		highlightedDiv.style.height = `${dims.height}px`
-		highlightedDiv.style.left = `${dims.left}px`
-		highlightedDiv.style.top = `${dims.top}px`
-		highlightedDiv.style.position = "absolute"
-		highlightedDiv.style.border = "2px solid lightgrey"
-        highlightedDiv.style.zIndex = "99999999"
-        highlightedDiv.id = "capture-element-highlighted-div"
-        highlightedDiv.style.pointerEvents = "none"
-        document.body.appendChild(highlightedDiv)
+		this.highlightedDiv = document.createElement("div")
+		this.highlightedDiv.style.borderRadius = `5px`
+		this.highlightedDiv.style.width = `${dims.width}px`
+		this.highlightedDiv.style.height = `${dims.height}px`
+		this.highlightedDiv.style.left = `${dims.left}px`
+		this.highlightedDiv.style.top = `${dims.top}px`
+		this.highlightedDiv.style.position = "absolute"
+		this.highlightedDiv.style.border = "2px solid lightgrey"
+        this.highlightedDiv.style.zIndex = "99999999"
+        this.highlightedDiv.id = "capture-element-highlighted-div"
+        this.highlightedDiv.style.pointerEvents = "none"
+        document.body.appendChild(this.highlightedDiv)
     }
 }
